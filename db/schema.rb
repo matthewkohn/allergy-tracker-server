4792905c2c6e3c0acab8cabde294c321868a6ab5
MattKohn <kohney@hotmail.com>
2022-05-12 11:04:04 -0600

    Migrate to schema

---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_05_12_165733) do
+
+  create_table "allergies", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_avoidable"
+    t.boolean "is_lethal"
+  end
+
+  create_table "dish_allergies", force: :cascade do |t|
+    t.integer "dish_id"
+    t.integer "allergy_id"
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
