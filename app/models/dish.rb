ea6e63e4375b010d3256107933b5e2be91d3e8df
matthewkohn <kohney319m@gmail.com>
2023-01-14 15:14:13 -0700

    Update post & patch in Dishes controller

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,14 +2,15 @@ class Dish < ActiveRecord::Base
   has_many :dish_allergies, dependent: :destroy
   has_many :allergies, through: :dish_allergies
   
-  # def allergy_data=(params)
-  #   self.ingredients.destroy_all
-  #   params.each do |hash|       
-  #     self.ingredients.new(
-  #       name: hash[:ingredient_name],
-  #       allergy_id: hash[:allergy_id]
-  #     )
-  #   end
-  # end
+  def allergy_data=(params)
+    self.dish_allergies.destroy_all
+    params.each do |hash|       
+      self.dish_allergies.new(
+        allergy_id: hash[:allergy_id],
+        ingredient_name: hash[:ingredient_name],
+        is_omittable: hash[:is_omittable]
+      )
+    end
+  end
 
 end
